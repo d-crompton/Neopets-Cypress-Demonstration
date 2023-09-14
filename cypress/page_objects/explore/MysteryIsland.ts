3101c3003867c50f9b67b27f394048a77a3cd9c3
Danny Crompton <danny.crompton@live.co.uk>
2023-09-14 20:38:32 +0800

    Adding Mystery Island PO and Tombola test.

---
+++ cypress/page_objects/explore/MysteryIsland.ts
@@ -0,0 +1,8 @@
+class MysteryIsland {
+  selector = {
+    liTombola: ".quickLinks__2020 > li:nth-of-type(16)",
+    inputPlayTomabola: 'input[value="Play Tombola!"]',
+  };
+}
+
+export const mysteryIsland = new MysteryIsland();
