d6d46467e7ab811b14f643c01c3480f057cd1cdf
Danny Crompton <danny.crompton@live.co.uk>
2023-12-18 09:45:05 +0800

    Adding not hungry status to fixture

--- cypress/fixtures/petStatus.ts
+++ cypress/fixtures/petStatus.ts
@@ -2,6 +2,7 @@ export const hungerStatuses = {
   // Red values
   dying: "Dying",
   // Black values
+  not_hungry: "not hungry",
   fine: "fine",
   satiated: "satiated",
   very_full: "very full",
