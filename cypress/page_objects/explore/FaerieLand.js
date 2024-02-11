6493107c74c2b268d17b6fd86167dc9eb4d22bb8
Danny Crompton <danny.crompton@live.co.uk>
2024-02-11 12:25:23 +0800

    Added error if the spring has been used in past 30 mins

--- cypress/page_objects/explore/FaerieLand.js
+++ cypress/page_objects/explore/FaerieLand.js
@@ -2,6 +2,7 @@ class FaerieLand {
   selectors = {
     liHealingSprings: `li[onclick="location.href = '/faerieland/springs.phtml'"]`,
     btnHealMyPets: "input[value='Heal my Pets']",
+    pAfterHealMyPets: ".theme-bg p:first-of-type", // Message that appears after pressing the button above
   };
 }
 
