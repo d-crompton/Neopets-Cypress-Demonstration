25da27cb8705daa24ff58f10fb1409c669f55098
Danny Crompton <danny.crompton@live.co.uk>
2024-02-20 15:32:30 +0800

    Revert "Changed .ts files to .js, updated explore file to remove argument data type. Ran each test to confirm it still works"
    This reverts commit da3c9126f0269775f5a26e3166346970e46918c9.
    
    --Reverting the commit as the .ts file extensions appear to make code
    easier by allowing autocomplete to be recognised by VS Code

--- cypress/fixtures/credentials.js
+++
@@ -1,8 +0,0 @@
-// File to store my account credentials, added to gitignore file
-
-const credentials = {
-  username: "crompytomp3",
-  password: "12uwNLgFgnLgGvLZaig3YQ12",
-};
-
-export default credentials;
