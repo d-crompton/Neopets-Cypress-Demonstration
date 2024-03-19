b411c032f36b935a8601fd57ff5c5b9224e89d49
d-crompton <danny.crompton@live.co.uk>
2024-03-19 11:17:04 +0800

    Added commands to press button and collect prize and tested. However, click button doesn't respond so will re-try with just clicking the wheel to start

--- cypress/e2e/wheels/wheel_of_knowledge.cy.ts
+++ cypress/e2e/wheels/wheel_of_knowledge.cy.ts
@@ -1,5 +1,4 @@
 // Wheel of Knowledge in Brightvale - Daily spin
-
 import { explore } from "../../page_objects/explore/_explore";
 import { brightvale } from "../../page_objects/explore/Brightvale";
 
@@ -8,6 +7,10 @@ describe("Wheel of Knowledge", () => {
     explore.explore(explore.strings.brightvale);
     cy.get(brightvale.selectors.liWheelOfKnowledge).click();
     // Spin wheel
+    // cy.get(brightvale.selectors.btnSpinWheel).click();
+    cy.get(brightvale.selectors.canvasWheel).click();
+    cy.wait(20000); // Count wheel and adjust
     // Collect prize
+    cy.get(brightvale.selectors.canvasWheel).click();
   });
 });
