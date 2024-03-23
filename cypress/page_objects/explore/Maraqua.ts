7504b9b13dc935c5830b8dd0b11d09c6a69cdd9f
d-crompton <danny.crompton@live.co.uk>
2024-03-23 13:00:07 +0800

    Creation of Maraqua PO file

---
+++ cypress/page_objects/explore/Maraqua.ts
@@ -0,0 +1,17 @@
+import { createLinkElement } from "../../support/navigation";
+
+class Maraqua {
+  area = "water";
+
+  selectors = {
+    // Ruins
+    liRuins: createLinkElement(this.area, "index_ruins"),
+    // Underwater Fishing
+    liFishingVortex: createLinkElement(this.area, "fishing"),
+    btnReelLine: 'input[value^="Reel"]',
+  };
+
+  url = {
+    fishingVortex: "water/fishing.phtml",
+  };
+}
