2b360df335eaa746ea39a93741f330e89054ecf9
d-crompton <danny.crompton@live.co.uk>
2024-03-29 07:13:55 +0800

    Updated homepage PO and the "Before" code to accommodate a new NeoPass pop-up. Tested on Daily scripts

--- cypress/page_objects/homepage/homepage.ts
+++ cypress/page_objects/homepage/homepage.ts
@@ -1,6 +1,7 @@
 // Homepage - https://www.neopets.com/home/
 class HomePage {
   selectors = {
+    neopassWindowClose: ".np-modal #psPopupExitButton",
     currentPet: ".slick-current",
     divPet: ".hp-carousel-pet", // A pet is there
     addPet: ".hp-carousel-addpet",
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,10 +1,13 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 import { login } from "./login";
+import { homePage } from "../page_objects/homepage/homepage";
 
 // Code to run before every test
 before(() => {
   login();
+  // Close NeoPass window
+  cy.get(homePage.selectors.neopassWindowClose).click();
   // Prevent Uncaught Exception SyntexError:',' when home page loads
   Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
