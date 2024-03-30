8319cfc6de5765ab7a26044d998b4cefb094bded
d-crompton <danny.crompton@live.co.uk>
2024-03-30 16:31:58 +0800

    Updated createLinkElement function to reduce string liength and just rely on target with no area

--- cypress/support/navigation.ts
+++ cypress/support/navigation.ts
@@ -1,8 +1,12 @@
 // Used when iteracting with 'li' elements to create links for each area
-export function createLinkElement(area: string, target: string) {
-  if (area === "") {
-    return `li[onclick="location.href = '/${target}.phtml'"]`;
-  } else {
-    return `li[onclick="location.href = '/${area}/${target}.phtml'"]`;
-  }
+// export function createLinkElement(area: string, target: string) {
+//   if (area === "") {
+//     return `li[onclick="location.href = '/${target}.phtml'"]`;
+//   } else {
+//     return `li[onclick="location.href = '/${area}/${target}.phtml'"]`;
+//   }
+// }
+
+export function createLinkElement(target: string) {
+  return `li[onclick*='${target}']`;
 }
