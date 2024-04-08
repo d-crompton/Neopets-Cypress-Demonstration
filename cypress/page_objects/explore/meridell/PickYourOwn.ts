31298c67aadefa1c3e8e0a7e9c0f879adaa6c0e1
d-crompton <danny.crompton@live.co.uk>
2024-04-08 15:35:42 +0800

    Added further notes to PO

--- cypress/page_objects/explore/meridell/PickYourOwn.ts
+++ cypress/page_objects/explore/meridell/PickYourOwn.ts
@@ -14,7 +14,44 @@ class PickYourOwn {
     btnCollectBerriesAndLeave: 'input[value*="Collect"]',
   };
 
-  boobyC;
+  boobyPrizes = {
+    "Pile of Dung": "id=19",
+    "Half-eaten Berry": "id=20",
+  };
+
+  /*
+  Recursive Function
+  Arguments:
+  > Number of Maps visited
+  > Number of berries collected
+  > Number of berries currently in punnet
+  
+  // Click map/Collect berry
+  // If there's 6 berries in the punnet, end the game
+  // Check that its not a booby prize
+  // If so, chuck it (use its id for img in punnet?)
+
+  // Move to next map (using next step obj)
+  // Figure out how many arrows are present. 
+
+  */
+
+  pickYourOwnGame(
+    mapsVisited: number,
+    berriesCollected: number,
+    berriesInPunnet: number
+  ) {
+    // Collect Berry
+    cy.get(this.selectors.imgFieldPicture).click();
+    berriesCollected += 1;
+    berriesInPunnet += 1;
+    // Check that its not a booby prize - Whilst you have less than 6 berries
+    if (berriesInPunnet < 6) {
+      // Remove booby prize
+    } else {
+      // When you have 6 and game ends
+    }
+  }
 }
 
 /*
@@ -22,7 +59,15 @@ When clicking the item you want to discard, it will ask a prompt with OK/Cancel
 Barbed Wire - id=15
 Rotten Berry - id=18
 Pile of Dung - id=19
-Half berry - id=20
+Half-eaten Berry - id=20
+*/
+
+/* 
+  Started in:
+  > Middle Left
+  > Center
+
+  // Maybe to work out where you are, use a string ("UDLR = Up, Down, Left, Right") to highlight available arrows
 */
 
 export const pickYourOwn = new PickYourOwn();
