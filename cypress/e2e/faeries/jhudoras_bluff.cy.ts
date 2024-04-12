5159b76022580cf7ef372b6bde155e327f0c075e
d-crompton <danny.crompton@live.co.uk>
2024-04-12 21:15:20 +0800

    Updated script - getting quest and searching shop wizard

--- cypress/e2e/faeries/jhudoras_bluff.cy.ts
+++ cypress/e2e/faeries/jhudoras_bluff.cy.ts
@@ -1,18 +1,27 @@
 // Jhudora's Bluff Faerie Quest - Faerieland
 import { explore } from "../../page_objects/explore/_explore";
 import { faerieLand } from "../../page_objects/explore/FaerieLand";
+import { neopiaCentral } from "../../page_objects/explore/NeopiaCentral";
 
 describe("Jhudora's Bluff", () => {
   it("Jhudora's Bluff", () => {
-    explore.explore(faerieLand.area);
+    explore.explore(explore.links.faerieLand);
     cy.get(faerieLand.selectors.liJhudora).click();
     cy.get(faerieLand.selectors.btnJhudoraAccept1).click();
     cy.get(faerieLand.selectors.btnJhudoraAccept2).click();
     cy.get(faerieLand.selectors.bJhudoraItem)
       .invoke("text")
-      .then((text) => {
+      .then((itemName) => {
         // Go get item
+        console.log(itemName);
+        cy.visit(neopiaCentral.urls.shopWizardSearch);
+        cy.get(neopiaCentral.selectors.inputShopWizard).type(itemName);
+        cy.get(neopiaCentral.selectors.divWizardSubmit).click();
+        // Throw an error if the item is not available - or try resubmitting
+        cy.get(neopiaCentral.selectors.aShopFirstResult).click();
+        cy.get(neopiaCentral.selectors.aHighlightedItem).click();
       });
+    cy.pause();
 
     /* Getting Item
       https://www.neopets.com/shops/wizard.phtml 
